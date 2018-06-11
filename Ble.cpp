ffd441082f2708420f8ca6d50000f4d96522e5c2
Lorenzo <l.dalessandro5@campus.unimib.it>
2018-06-11 08:31:32 +0200

    modificati commenti

--- Ble.cpp
+++ Ble.cpp
@@ -7,16 +7,21 @@ Ble::Ble(SoftwareSerial *ss)
 	_btSerial = ss;
 }
 
+//inizializza la seriale bluetooth
+//baud: velocità di trasmissione
 void Ble::bStart(int baud){
    delay (1000);
   _btSerial -> begin(baud);
 }
 
+//scrive una stringa sulla seriale bluetooth
 void Ble::bWrite(const String data)
 {
 	_btSerial -> print(data);
 }
 
+//legge dalla seriale bluetooth finchè ci sono dati disponibili
+//restituisce un unica stringa contenente tutti i dati letti
 String Ble::bRead()
 {
 	String s;
